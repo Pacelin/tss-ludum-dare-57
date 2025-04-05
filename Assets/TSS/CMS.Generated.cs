9ae910809dc096e8e5fbed8fe35387fee133844d
Pacelin <Warfacer2020@gmail.com>
2025-04-05 15:12:00 +0300

    feat: Pause, Social

--- Assets/TSS/CMS.Generated.cs
+++ Assets/TSS/CMS.Generated.cs
@@ -21,8 +21,20 @@ namespace TSS.ContentManagement
 
         public async UniTask Initialize(CancellationToken cancellationToken)
         {
+			await Scenes.Initialize(cancellationToken);
         }
 
         public void Dispose() { }
+
+		[PublicAPI]
+		public static class Scenes
+		{
+			public const string MainMenu = "Assets/Scenes/1_Menu.unity";
+			public const string Game = "Assets/Scenes/2_Game.unity";
+
+			public static async UniTask Initialize(CancellationToken cancellationToken)
+			{
+			}
+		}
     }
 }
---
+++ Assets/_Project/Scripts/UI/SocialButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using R3;
+using UnityEngine;
+
+namespace LudumDare57.UI
+{
+    [RequireComponent(typeof(FeedbackButton))]
+    public class SocialButton : MonoBehaviour
+    {
+        [SerializeField] private FeedbackButton _feedbackButton;
+        [SerializeField] private string _url;
+        
+        private IDisposable _disposable;
+
+        private void OnValidate()
+        {
+            if (!_feedbackButton)
+                _feedbackButton = GetComponent<FeedbackButton>();
+        }
+
+        private void OnEnable()
+        {
+            _disposable = _feedbackButton.ObserveClick()
+                .Subscribe(_ => Application.OpenURL(_url));
+        }
+
+        private void OnDisable()
+        {
+            _disposable?.Dispose();
+        }
+    }
+}
