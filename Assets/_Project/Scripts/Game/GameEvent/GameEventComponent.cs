bd142af96bdcadb92cf7ff2d12b57314d7f7362b
Pacelin <Warfacer2020@gmail.com>
2025-04-06 19:57:56 +0300

    feat: Events

--- Assets/_Project/Scripts/Game/GameEvent/GameEventComponent.cs
+++ Assets/_Project/Scripts/Game/GameEvent/GameEventComponent.cs
@@ -1,24 +1,44 @@
 ﻿using System.Linq;
+using Cysharp.Threading.Tasks;
 using mixpanel;
 using R3;
+using TMPro;
 using TSS.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LudumDare57.Game.GameEvent
 {
     public class GameEventComponent : MonoBehaviour
     {
         [SerializeField] private GameEventOptionComponent[] _options;
+        [SerializeField] private TMP_Text _resultText;
         [SerializeField] private ScriptableTween _openTween;
+        [SerializeField] private ScriptableTween _showResultTween;
+        [SerializeField] private ScriptableTween _hideTween;
+        [SerializeField] private Button _clickToContinue;
         
         private CompositeDisposable _disposables;
         private string _eventAnalytics;
+        private bool _selected;
         
         private void OnEnable()
         {
+            _selected = false;
             _disposables = new();
             foreach (var option in _options)
                 StartOption(option);
+            _clickToContinue.OnClickAsObservable().Subscribe(_ =>
+            {
+                if (!_selected)
+                    return;
+                if (_showResultTween.IsPlaying)
+                    return;
+                Mixpanel.Track(_eventAnalytics);
+                GameContext.Hole.SetStop(false);
+                _hideTween.Play();
+                _hideTween.WaitWhilePlay().ContinueWith(() => Destroy(gameObject)).Forget();
+            }).AddTo(_disposables);
         }
 
         private void OnDisable()
@@ -28,6 +48,7 @@ namespace LudumDare57.Game.GameEvent
 
         public void StartEvent(string eventAnalytics)
         {
+            GameContext.Hole.SetStop(true);
             _eventAnalytics = eventAnalytics;
             Mixpanel.StartTimedEvent(eventAnalytics);
             _openTween.Play();
@@ -48,6 +69,11 @@ namespace LudumDare57.Game.GameEvent
             option.Button.interactable = enableOption;
             option.Button.OnClickAsObservable().Subscribe(_ =>
             {
+                if (_selected)
+                    return;
+                if (_openTween.IsPlaying)
+                    return;
+                _selected = true;
                 if (option.CoinsPenalty > 0)
                     GameContext.Coins.Value = Mathf.Max(0, GameContext.Coins.Value - option.CoinsPenalty);
                 foreach (var itemPenalty in option.Penalty)
@@ -66,9 +92,10 @@ namespace LudumDare57.Game.GameEvent
                 }
                 if (_openTween.IsPlaying)
                     _openTween.Pause();
-                option.ShowResultTween.Play();
+
+                _resultText.text = option.Result.GetLocalizedString();
+                _showResultTween.Play();
                 Mixpanel.Track(option.SelectionAnalytics);
-                Mixpanel.Track(_eventAnalytics);
             }).AddTo(_disposables);
         }
     }
