1d57f6841af04566f3e1a43e6d0b4f095096d161
Pacelin <Warfacer2020@gmail.com>
2025-04-06 14:35:18 +0300

    feat: Shop Analytics

--- Assets/_Project/Scripts/Game/Shop/ShopActivation.cs
+++ Assets/_Project/Scripts/Game/Shop/ShopActivation.cs
@@ -5,10 +5,12 @@ namespace LudumDare57.Game.Shop
     public class ShopActivation : ActivationTrigger
     {
         [SerializeField] private int _shopLevel;
+        [SerializeField] private string _enterAnalytics;
+        [SerializeField] private string _exitAnalytics;
         
         protected override void OnActivate()
         {
-            GameContext.Shop.Show(_shopLevel);
+            GameContext.Shop.Show(_shopLevel, _enterAnalytics, _exitAnalytics);
         }
     }
 }
